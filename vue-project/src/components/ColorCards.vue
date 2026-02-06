664022df5a025accb57da5f7c229b83e224537a1
wendylin84 <wendyl84@nycstudents.net>
2026-02-06 14:02:22 -0500

    jdije

---
+++ vue-project/src/components/ColorCards.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>{{ animal.name }}</h2>
+    <h3>{{ animal.species }}</h3>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  animal: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 200px;
+  width: 150px;
+  border: 3px solid rgba(0, 0, 0, 1);
+}
+</style>
